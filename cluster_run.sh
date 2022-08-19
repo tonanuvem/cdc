ad930f09bfd3c092e6f51936caa584b129932580
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-19 08:05:18 -0300

    Update cluster_run.sh

--- cluster_run.sh
+++ cluster_run.sh
@@ -6,21 +6,31 @@ docker stack deploy -c docker-stack.yml cdc
 
 echo ""
 echo "Aguardando a configuração do Debezium CDC (Change Data Capture)."
-while [ "$(docker logs cdc_connect_1 2>&1 | grep "Finished starting connectors and tasks" | wc -l)" != "1" ]; do
+while [ "$(docker service logs cdc_connect 2>&1 | grep "Finished starting connectors and tasks" | wc -l)" != "1" ]; do
   printf "."
   sleep 1
 done
 echo ""
 sh config_cdc.sh
+echo ""
+echo "Aguardando todos os 12 serviços entrarem em estado Running."
+while [ "$(docker stack ps cdc 2>&1 | grep "Running" | wc -l)" != "12" ]; do
+  printf "."
+  sleep 1
+done
+docker service ls
+echo ""
 echo "Config OK"
 IP=$(curl -s checkip.amazonaws.com)
 echo ""
 echo "URLs do projeto:"
 echo ""
-echo " - KAFKA UI        : $IP:8070"
+echo " - VISUALIZADOR    : http://$IP:8060"
 echo ""
-echo " - PostGres UI     : $IP:8080 ( login = admin@admin.com / senha = admin )" 
-echo " - MySQL UI        : $IP:8082 ( login = admin / senha = admin )"
+echo " - KAFKA UI        : http://$IP:8070"
 echo ""
-echo " - NIFI            : $IP:8090/nifi"
+echo " - PostGres UI     : http://$IP:8080 ( login = admin@admin.com / senha = admin )" 
+echo " - MySQL UI        : http://$IP:8082 ( login = admin / senha = admin )"
+echo ""
+echo " - NIFI            : http://$IP:8090/nifi"
 echo ""
