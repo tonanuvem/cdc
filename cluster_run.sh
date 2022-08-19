71796fb51b33bf83e954a59fe5a6fe7764de156f
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-19 07:55:41 -0300

    Create cluster_run.sh

---
+++ cluster_run.sh
@@ -0,0 +1,26 @@
+# Liberando a porta no caso de rodar no Cloud9
+sudo service mysql stop
+chmod 755 postgres_init_database.sh
+
+docker stack deploy -c docker-stack.yml cdc
+
+echo ""
+echo "Aguardando a configuração do Debezium CDC (Change Data Capture)."
+while [ "$(docker logs cdc_connect_1 2>&1 | grep "Finished starting connectors and tasks" | wc -l)" != "1" ]; do
+  printf "."
+  sleep 1
+done
+echo ""
+sh config_cdc.sh
+echo "Config OK"
+IP=$(curl -s checkip.amazonaws.com)
+echo ""
+echo "URLs do projeto:"
+echo ""
+echo " - KAFKA UI        : $IP:8070"
+echo ""
+echo " - PostGres UI     : $IP:8080 ( login = admin@admin.com / senha = admin )" 
+echo " - MySQL UI        : $IP:8082 ( login = admin / senha = admin )"
+echo ""
+echo " - NIFI            : $IP:8090/nifi"
+echo ""
