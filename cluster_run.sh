9b34b1b3e055e237e29e5cc3bbb98abef4850de3
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-19 23:30:47 -0300

    Update cluster_run.sh

--- cluster_run.sh
+++ cluster_run.sh
@@ -7,11 +7,11 @@ while [ "$(docker stack ps --no-trunc cdc 2>&1 | grep "Running         Running" 
   sleep 1
 done
 echo ""
-echo "Aguardando a configuração do Debezium CDC (Change Data Capture)."
-while [ "$(docker logs cdc_connect_1 2>&1 | grep "Finished starting connectors and tasks" | wc -l)" != "1" ]; do
-  printf "."
-  sleep 1
-done
+#echo "Aguardando a configuração do Debezium CDC (Change Data Capture)."
+#while [ "$(docker logs cdc_connect_1 2>&1 | grep "Finished starting connectors and tasks" | wc -l)" != "1" ]; do
+#  printf "."
+#  sleep 1
+#done
 echo ""
 sh config_cdc.sh
 echo ""
