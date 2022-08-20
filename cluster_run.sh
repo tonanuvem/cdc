34d4ef37a6bc2852e719c7e44ef170e47ad19047
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-19 22:30:26 -0300

    Update cluster_run.sh

--- cluster_run.sh
+++ cluster_run.sh
@@ -7,7 +7,7 @@ docker stack deploy -c docker-stack.yml cdc
 echo ""
 echo "Aguardando os serviços entrarem em estado Running."
 
-while [ "$(docker stack ps --no-trunc cdc 2>&1 | grep cdc_connect | grep Running | grep Running | wc -l)" != "1" ]; do
+while [ "$(docker stack ps cdc 2>&1 | grep cdc_connect | grep Running | grep Running | wc -l)" != "1" ]; do
   printf "."
   sleep 1
 done
