92180d9361b84cb92146cd482476a47b0c0774b8
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-01 02:00:25 -0300

    Update build_images.sh

--- build_images.sh
+++ build_images.sh
@@ -1,7 +1,8 @@
 # Cria as imagens com os arquivos necessários, evitando usado de volumes para mapear esses arquivos
 
 docker build -t tonanuvem/kowl:conf -f ./Dockerfile/Dockerfile.tonanuvem.kowl .
-docker build -t tonanuvem/mysql:usuarios -f ./Dockerfile/Dockerfile.mysql.usuarios .
+docker build -t tonanuvem/mysql:usuarios_v5.7 -f ./Dockerfile/Dockerfile.mysql.usuarios_v5.7 .
+docker build -t tonanuvem/mysql:usuarios_v9 -f ./Dockerfile/Dockerfile.mysql.usuarios_v9 .
 docker build -t tonanuvem/postgres:produtos -f ./Dockerfile/Dockerfile.postgres.produtos .
 docker build -t tonanuvem/nifi:cdc -f ./Dockerfile/Dockerfile.nifi.cdc .
 docker build -t tonanuvem/pgadmin:cdc -f ./Dockerfile/Dockerfile.pgadmin.cdc .
