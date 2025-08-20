946820bd7cee24d3a6d8aa0297fdd8b91e21ae2e
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-08-20 16:42:16 -0300

    Update airflow_run.sh

--- airflow_run.sh
+++ airflow_run.sh
@@ -2,6 +2,9 @@
 
 # https://airflow.apache.org/docs/apache-airflow/stable/howto/docker-compose/index.html
 
+# https://medium.com/apache-airflow/data-engineering-end-to-end-project-part-1-airflow-kafka-cassandra-mongodb-docker-a87f2daec55e
+# https://medium.com/apache-airflow/data-engineering-end-to-end-project-part-2-airflow-kafka-cassandra-mongodb-docker-52a2ec7113de
+
 # Download the docker-compose.yaml file
 # wget  -O docker-compose-airflow.yaml 'https://airflow.apache.org/docs/apache-airflow/stable/docker-compose.yaml'
 # curl -LfO 'https://airflow.apache.org/docs/apache-airflow/3.0.4/airflow.sh'
