a42445c6b72ee4fb6c0d0d74e757f9d149597d3d
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-08-21 15:21:12 -0300

    Update airflow_run.sh

--- airflow_run.sh
+++ airflow_run.sh
@@ -38,7 +38,7 @@ docker exec -it cdc-airflow-scheduler-1 airflow connections add kafka_airflow_te
     --conn-type kafka \
     --conn-host kafka \
     --conn-port 9092 \
-    --conn-extra '{"bootstrap.servers": "kafka:9092", "group.id": "kafka_airflow_teams", "security.protocol": "PLAINTEXT", "auto.offset.reset": "beginning"}' 2>/dev/null
+    --conn-extra '{"bootstrap.servers": "kafka:9092", "group.id": "TEAMS_FIAP_AIRFLOW, "security.protocol": "PLAINTEXT", "auto.offset.reset": "beginning"}' 2>/dev/null
 
 IP=$(curl -s checkip.amazonaws.com)
 echo ""
