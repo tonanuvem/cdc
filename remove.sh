2eb0945a431324d976b59f113156df2f7873f947
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2026-04-01 10:47:41 -0300

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,2 +1,3 @@
 docker-compose stop && docker-compose rm -f && docker volume prune -f && git pull
 docker volume rm cdc_esdata cdc_mysqldata cdc_pgadmindata cdc_postgresdata
+docker volume rm cdc_api_tokens cdc_cloudbeaver-postgres_data cdc_cloudbeaver_certs cdc_trusted_cacerts dbeaver_api_tokens dbeaver_cloudbeaver_certs dbeaver_trusted_cacerts
